8f97b52e59855c6163b89dddedbfbb87e4a71a83
Mathias Talo <mathias.talo@outlook.com>
2023-02-14 22:52:44 +0200

    Rewrote and finished JS, Added dark background. Rewrote the JavaScript file, finished it and now I will perfect it and make it more readable and easier. Also added simple styles to background and text for now (Dark Mode :D).

--- app.js
+++ app.js
@@ -1,23 +1,81 @@
-// Set the scores of the player and computer to 0
-let playerScore = 0;
-let computerScore = 0;
-
 // Array of options the computer can choose from
-let choicesForPC = ['rock', 'paper', 'scissors'];
+const options = ['rock', 'paper', 'scissors'];
 
 // Function that makes the computer choose from the provided array
 function computerChoice() {
-    let randomNum = Math.floor(Math.random() * choicesForPC.length);
-    let cChoice = choicesForPC[randomNum];
+    let randomNum = Math.floor(Math.random() * options.length);
+    let cChoice = options[randomNum];
     return cChoice;
 };
 
-// Function that gets the players input to the prompt
+// Function that gets the players input to the prompt, puts it in lowercase and checks if it exists in the options array
 function playerChoice() {
-    let playerSelection = prompt('Enter your choice:');
-    let pChoice = playerSelection.toLowerCase();         // Format the players choice to lowercase
-    return pChoice;
+    let validatedInput = false;
+    while (validatedInput == false) {
+        const choice = prompt('Choose Rock, Paper or Scissors');
+        if (choice == null) {
+            continue;
+        }
+        const choiceInLower = choice.toLowerCase();
+        if (options.includes(choiceInLower)) {
+            validatedInput = true;
+            return choiceInLower;
+        }
+    }
 };
 
- console.log(playerChoice());
- console.log(computerChoice());
+// Function to check for round outcome
+function checkWinner(player, computer) {
+    if (player == computer) {
+        return 'tie'
+    } else if (
+        (player == 'rock' && computer == 'scissors') ||
+        (player == 'scissors' && computer == 'paper') ||
+        (player == 'paper' && computer == 'rock')
+        ) {
+            return 'player';
+        } else {
+            return 'computer';
+        }
+}
+
+// Function that plays a single round of RPS
+function playRound(player, computer) {
+    const result = checkWinner(player, computer);
+
+    if (result == 'tie') {
+        return 'It`s a Tie!'
+    } else if (result == 'player') {
+        return `You Win! ${player} beats ${computer}`
+    } else {
+        return `You Lose! ${computer} beats ${player}`
+    }
+};
+
+// Function to play game and report if the game is won or lost
+function game() {
+    let playerScore = 0;
+    let computerScore = 0;
+
+    for (let i = 0; i < 5; i++) {
+        const player = playerChoice();
+        const computer = computerChoice();
+        console.log(playRound(player, computer));
+        if (checkWinner(player, computer) == 'player') {
+            playerScore++;
+        } else if (checkWinner(player, computer) == 'computer') {
+            computerScore++;
+        }
+    }
+    
+    if (playerScore > computerScore) {
+        console.log('You Won The Game!');
+    } else if (playerScore < computerScore) {
+        console.log('You Lost The Game :(');
+    } else {
+        console.log('The Game Was a Tie');
+    }
+}
+
+// Call for game to be played on opening the website
+game();
